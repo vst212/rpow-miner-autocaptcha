f63779e11075465cda564fe42662e3b73920896f
vst212 <vst212@github>
2026-05-10 15:30:33 +0800

    perf: increase post-mint cooldown to 3s to avoid server 429

--- rpow-cli.js
+++ rpow-cli.js
@@ -222,7 +222,7 @@ function mineRetryDelayMs(failures, err) {
 }
 
 function postMintDelayMs() {
-  return 2000 + Math.floor(Math.random() * 350);
+  return 3000 + Math.floor(Math.random() * 350);
 }
 
 function loadState(file) {
